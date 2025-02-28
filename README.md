1484eaa76f3ece56ec70a34467dcfe09d286af58
Samuel Tavernier <staverni@student.42luxembourg.lu>
2025-02-28 17:15:27 +0100

    better organisation and readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# So Long Tester
+## How to use the tester:
+Make your project<br>
+Simply clone the repository in your project folder<br>
+`git clone https://github.com/samtvn/so_long_tester.git`
+Launch the tester from your project directory<br>
+`./so_long_tester/test_broken.sh`<br>
+One liner:<br>
+`make fclean && make && [ -d so_long_tester ] || git clone https://github.com/samtvn/so_long_tester.git && ./so_long_tester/test_broken.sh`<br>
+## How to interpret the output
+No Valgrind output means noleaks!<br>
+If there are Valgrind outputs, then it is easy. There are leaks.<br>
+You should check that it is the user's fault and not the minilibx.<br>
+Normally, it should be the user's fault.<br>
+<br>
+<span style="color:red">red</span> does not mean it is KO, I just chose the output to be red. You should check each error output to ensure the user specifies the nature of the error as required by the subject.<br>
+<br>
+The three last tests require you to press ESC, the X button and then to test the game normally.<br>
+You should certainly do the ESC and X tests again by yourself to check after taking collectibles too.
--- test_broken.sh
+++ test_broken.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
 
-MAP_DIR="resources/maps"
-VALID_MAP="resources/maps/map.ber"
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+MAP_DIR="$SCRIPT_DIR/maps"
+VALID_MAP="$MAP_DIR/map.ber"
 VALGRIND="valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --quiet"
-EXECUTABLE_PATH="../so_long"
+EXECUTABLE_PATH="$SCRIPT_DIR/../so_long"
+
+# Function to print errors in red
+print_red() {
+    tput setaf 1  # Set text color to red
+    echo "$1"
+    tput sgr0     # Reset text formatting
+}
 
 for map in "$MAP_DIR"/broken*.ber; do
     if [ -f "$map" ]; then
         echo "Launching ./so_long with map: $map"
-        $VALGRIND $EXECUTABLE_PATH "$map" 2>&1 | sed $'s/^/\033[31m/' | sed $'s/$/\033[0m/'
+        output=$($VALGRIND $EXECUTABLE_PATH "$map" 2>&1)
+        print_red "$output"
     fi
 done
 
 echo "Launching ./so_long without arguments"
-$VALGRIND $EXECUTABLE_PATH 2>&1 | sed $'s/^/\033[31m/' | sed $'s/$/\033[0m/'
+output=$($VALGRIND $EXECUTABLE_PATH 2>&1)
+print_red "$output"
 
 echo "Launching ./so_long with > 1 argument"
-$VALGRIND $EXECUTABLE_PATH $VALID_MAP other 2>&1 | sed $'s/^/\033[31m/' | sed $'s/$/\033[0m/'
+output=$($VALGRIND $EXECUTABLE_PATH $VALID_MAP other 2>&1)
+print_red "$output"
 
 echo "Launching ./so_long with unexisting file"
-$VALGRIND $EXECUTABLE_PATH awesome_map.ber 2>&1 | sed $'s/^/\033[31m/' | sed $'s/$/\033[0m/'
+output=$($VALGRIND $EXECUTABLE_PATH awesome_map.ber 2>&1)
+print_red "$output"
 
-echo "Launching ./so_long with valid map: $MAP, simulating ESC exit"
-$VALGRIND $EXECUTABLE_PATH "$VALID_MAP" &
-PID=$!
-echo "Press ESC to exit the game"
-wait $PID
+echo "Launching ./so_long with valid map: $VALID_MAP, simulating ESC exit"
+output=$($VALGRIND $EXECUTABLE_PATH "$VALID_MAP" 2>&1)
+print_red "$output"
 
-echo "Launching ./so_long with valid map: $MAP, simulating close button exit"
-$VALGRIND $EXECUTABLE_PATH "$VALID_MAP" &
-PID=$!
-echo "Press window X button to exit the game"
-wait $PID
+echo "Launching ./so_long with valid map: $VALID_MAP, simulating close button exit"
+output=$($VALGRIND $EXECUTABLE_PATH "$VALID_MAP" 2>&1)
+print_red "$output"
 
-echo "Launching ./so_long with valid map: $MAP"
-$VALGRIND $EXECUTABLE_PATH "$VALID_MAP" 2>&1 | sed $'s/^/\033[31m/' | sed $'s/$/\033[0m/'
+echo "Launching ./so_long with valid map: $VALID_MAP"
+output=$($VALGRIND $EXECUTABLE_PATH "$VALID_MAP" 2>&1)
+print_red "$output"
